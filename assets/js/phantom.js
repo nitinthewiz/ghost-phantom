44e73198fac380c6c96c5428c78652d0ddf5a9ab
Hayden Bleasel <haydenbleasel@gmail.com>
2014-07-15 10:54:13 +1000

    Executed FastClick, new asset references

---
+++ assets/js/phantom.js
@@ -0,0 +1,12 @@
+/*jslint browser:true*/
+/*global FastClick*/
+
+(function () {
+
+    'use strict';
+
+    window.addEventListener('load', function () {
+        FastClick.attach(document.body);
+    }, false);
+
+}());
