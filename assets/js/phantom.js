ac65945121ff935a1925c37045f93cd718379f00
Hayden Bleasel <haydenbleasel@gmail.com>
2014-07-15 11:01:17 +1000

    Converted to jQuery for simplicity

--- assets/js/phantom.js
+++ assets/js/phantom.js
@@ -1,12 +1,7 @@
 /*jslint browser:true*/
-/*global FastClick*/
+/*global $, FastClick*/
 
-(function () {
-
+$(function () {
     'use strict';
-
-    window.addEventListener('load', function () {
-        FastClick.attach(document.body);
-    }, false);
-
-}());
+    FastClick.attach(document.body);
+});
