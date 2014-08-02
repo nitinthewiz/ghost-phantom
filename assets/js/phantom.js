fcbafc18acb8205d5a7b47e2744d7965c425a227
Hayden Bleasel <haydenbleasel@gmail.com>
2014-08-02 11:33:24 +1000

    Remove inline JavaScript

--- assets/js/phantom.js
+++ assets/js/phantom.js
@@ -4,4 +4,19 @@
 $(function () {
     'use strict';
     FastClick.attach(document.body);
+
+    $('#twitter').click(function () {
+        window.open(this.href, 'twitter-share', 'width=550,height=235');
+        return false;
+    });
+
+    $('#facebook').click(function () {
+        window.open(this.href, 'facebook-share', 'width=580,height=296');
+        return false;
+    });
+
+    $('#google-plus').click(function () {
+        window.open(this.href, 'google-plus-share', 'width=490,height=530');
+        return false;
+    });
 });
