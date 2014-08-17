a8780680b88d922aed9309a562d6b9cf646e220c
Hayden Bleasel <haydenbleasel@gmail.com>
2014-08-17 20:40:22 +1000

    Added parallax scrolling on header

--- assets/js/phantom.js
+++ assets/js/phantom.js
@@ -1,14 +1,35 @@
-/*jslint browser:true*/
+/*jslint browser:true, unparam:true*/
 /*global $, FastClick*/
 
 $(function () {
 
     'use strict';
 
+    // Execute FastClick.js
     FastClick.attach(document.body);
 
+    // Expanded article images
     $('article img').parent().addClass('article-image');
 
+    // Custom transform and opacity modifier for Stellar.js
+    $.stellar.positionProperty.transfade = {
+        setPosition: function (element, newLeft, originalLeft, newTop, originalTop) {
+            var distance = newTop - originalTop,
+                rate = $('header').height() / 5;
+            element.css('transform', 'translate3d(0, ' + distance + 'px, 0').css('opacity', 1 - (distance / rate));
+        }
+    };
+
+    // Execute Stellar.js
+    $.stellar({
+        horizontalScrolling: false,
+        responsive: true,
+        positionProperty: "transfade",
+        parallaxBackgrounds: false,
+        verticalOffset: 90
+    });
+
+    // Social sharing links
     $('#twitter').click(function () {
         window.open(this.href, 'twitter-share', 'width=550,height=235');
         return false;
